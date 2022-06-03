8d1cc4a823e892e72597736688a706ac4ceb35a5
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2022-06-03 18:35:52 -0300

    estabelecimento dos objetivos

---
+++ script.js
@@ -0,0 +1,5 @@
+//Objetivos:
+//1 - Garantir que a tela(API do DOM) tenha sido carregada por inteiro
+//2 - Função que crie parágrafos 
+//3 - Capturar todos os botões da tela
+//4 - Fazer iteração para que todos os botões quando clicados criem um parágrafo
