f45bf8a7d01931f2a1e0c0a8e17146ec3962c040
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2022-06-03 18:55:25 -0300

    criacao da iteracao que percorre todos os buttons e dispara funcao de geracao de paragrafos quando botao e clicado e evento disparado

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //1 - Garantir que a tela(API do DOM) tenha sido carregada por inteiro - OK
 //2 - Função que crie parágrafos - OK
 //3 - Capturar todos os botões da tela - OK
-//4 - Fazer iteração para que todos os botões quando clicados criem um parágrafo
+//4 - Fazer iteração para que todos os botões quando clicados criem um parágrafo - OK
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -12,4 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.appendChild(para); //Gruda o p da const no body(elemento pai)
     }
     const buttons = document.querySelectorAll('button'); //Retorna nodeLists do tipo static(que são array-like objects, portanto iteráveis)
+
+    for(button of buttons) {
+        button.addEventListener('click', createParagraph); //Cada 'button' chama a função 'createParagraph()' ao ser clicado
+    }
 });
