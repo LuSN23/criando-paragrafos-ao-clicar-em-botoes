09537e8ec752b6188f3b605cfc8a729d53b78a2b
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2022-06-03 18:39:10 -0300

    garantia do carregamento completo da pagina

--- script.js
+++ script.js
@@ -3,3 +3,8 @@
 //2 - Função que crie parágrafos 
 //3 - Capturar todos os botões da tela
 //4 - Fazer iteração para que todos os botões quando clicados criem um parágrafo
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+});
